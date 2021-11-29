1db9d89288b8b1cd7c6e23177627f2ad0069ea11
0xNino <86327707+0xNino@users.noreply.github.com>
2021-11-29 13:42:51 +0100

    local

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,18 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:28:25 by 0xNino            #+#    #+#             */
-/*   Updated: 2021/11/24 13:20:11 by 0xNino           ###   ########.fr       */
+/*   Updated: 2021/11/24 15:06:39 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdarg.h>
 
 char	*get_next_line(int fd);
 
